a5982c771e5b77048741d7157dac46786db0f7c4
Preeti <preetisilla18@gmail.com>
2025-03-11 16:03:50 +0530

    Initial Selenium Framework

--- src/test/java/com/thetestingacademy/TestSelenium/TestSelenium.java
+++ src/test/java/com/thetestingacademy/TestSelenium/TestSelenium.java
@@ -1,4 +1,15 @@
 package com.thetestingacademy.TestSelenium;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
 public class TestSelenium {
+
+    @Test
+    public void test_VWOLogin(){
+        WebDriver driver = new EdgeDriver();
+        driver.get("https://app.vwo.com/#/login");
+        driver.quit();
+    }
 }
